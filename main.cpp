dfcdf6f93d11511ef0cdd31185a803522f8fb250
hubenchang0515 <hubenchang0515@outlook.com>
2019-09-27 18:52:40 +0800

    add libclamav

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QMessageLogContext>
+#include <clamav/clamav.h>
 
 int main(int argc, char *argv[])
 {
+    cl_init(CL_INIT_DEFAULT);
+    struct cl_engine* clEngine = cl_engine_new();
+    if(clEngine == nullptr)
+    {
+        return 1;
+    }
+
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
@@ -16,5 +25,5 @@ int main(int argc, char *argv[])
     }, Qt::QueuedConnection);
     engine.load(url);
 
-    return app.exec();
+    return app.exec() && cl_engine_free(clEngine);
 }
